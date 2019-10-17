07dd20d9b1cd6dd5fedef8d6f1d8b2727ab97a7f
Chris Lucynski <chris.lucynski@gmail.com>
2019-10-16 20:41:13 -0600

    added movie constructor, included navbar in handlebars, added schema

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+-- TESTING DATA
+INSERT INTO(movie_title, watched, summary, actors, release_year, rt_rating, user_rating)
+    VALUES('The Matrix', '1', 'the internet mesiah goes to town on agent smith', 'Keanu Reeves', '2002', '87%', '4');
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,11 @@
 var movies = require("../models/movies");
+var movieTemplate = require('../models/omdb')
+var omdb = require('omdb-client')
 
 module.exports = function(app) {
   // Get all examples
   app.get("/movies", function(req, res) {
+    
    res.send('get all movies')
   });
 
