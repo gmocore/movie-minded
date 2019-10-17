6bd33b20d3c5415b4a831be1ca9b4ad9f90c4ace
gerritt <gmocore@gmail.com>
2019-10-16 19:41:42 -0600

    basic routes setup

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,22 @@
-var db = require("../models");
+var movies = require("../models/movies");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  app.get("/movies", function(req, res) {
+   res.send('get all movies')
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/movies/watched", function(req, res) {
+    res.send('watched movies')
+  });
+
+  app.post("/movies/unwatched", function(req, res) {
+    res.send('unwatched movies')
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+   
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,22 @@
-var db = require("../models");
+var movies = require("../models/movies");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  app.get("/home", function(req, res) {
+    res.send("this is the home route");
+  });
+
+  app.get("/movies/unwatched", function(req, res) {
+    res.send("this is the unwatched route");
+  });
+  
+  app.get("/movies/watched", function(req, res) {
+    res.send("this is the watched route");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/movies/:id", function(req, res) {
+    //
   });
 
   // Render 404 page for any unmatched routes
