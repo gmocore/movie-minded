fa6b7cc9ee4b0c13d3a3cfebc1bddd9e5b508e48
gerritt <gmocore@gmail.com>
2019-10-16 20:37:00 -0600

    added .env, modified api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,34 +6,11 @@ module.exports = function(app) {
    res.send('get all movies')
   });
 
-  // Create a new example
   app.post("/movies/watched", function(req, res) {
     res.send('watched movies')
   });
 
   app.post("/movies/unwatched", function(req, res) {
     res.send('unwatched movies')
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-   
-
-
-  // Create a new example
-  router.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  router.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-
   });
 };
