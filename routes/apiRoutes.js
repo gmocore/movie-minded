9eef8f554c931c0c239b217d9e4b965f007dbd10
Chris Lucynski <chris.lucynski@gmail.com>
2019-10-17 21:17:32 -0600

    refined displayed content for watched and unwatched

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,13 @@ var movies = require("../models/movies");
 var movieTemplate = require("../models/omdb");
 var omdb = require("omdb-client");
 
-const OmdbApiClient = require('open-movie-database-api').OmdbApiClient;
-const client = new OmdbApiClient('trilogy');
+const OmdbApiClient = require("open-movie-database-api").OmdbApiClient;
+const client = new OmdbApiClient("trilogy");
 
-
- function Movie(
+function Movie(
   movieTitle,
   watched,
+  poster,
   summary,
   actors,
   releaseYear,
@@ -16,12 +16,12 @@ const client = new OmdbApiClient('trilogy');
 ) {
   this.movieTitle = movieTitle;
   this.watched = watched;
+  this.poster = poster;
   this.summary = summary;
   this.actors = actors;
   this.releaseYear = releaseYear;
   this.rtRating = rtRating;
-  
-};
+}
 
 module.exports = function(app) {
   app.post("/movies/watched/:id", function(req, res) {
@@ -36,18 +36,27 @@ module.exports = function(app) {
 
   app.post("/test", function(req, res) {
     // res.render('ondeck')
-    console.log(req.body)
+    console.log(req.body);
     client.getByTitle(req.body.movie_title).then(result => {
-      console.log(result.Title)
+      console.log(result.Title);
       let movie = new Movie(
         result.Title,
         false,
+        result.Poster,
         result.Plot,
         result.Actors,
         result.Year,
         result.Ratings[1].Value
       );
-      movies.add(movie.movieTitle, movie.summary, movie.actors, movie.releaseYear, movie.rtRating, () => res.redirect("/unwatched"))
+      movies.add(
+        movie.movieTitle,
+        movie.poster,
+        movie.summary,
+        movie.actors,
+        movie.releaseYear,
+        movie.rtRating,
+        () => res.redirect("/movies/unwatched")
+      );
       // return res.send(movie)
     });
   });
